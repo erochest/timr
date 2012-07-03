cd59a42663b3b1e8880c5b14011007457ddb21ab
Eric Rochester <err8n@virginia.edu>
2012-07-03 09:57:59 -0400

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='timr',
+        version='0.0.0',
+        description='Profiling web requests.',
+        author='Eric Rochester',
+        author_email='erochest@virginia.edu',
+        url='https://github.com/erochest/timr',
+        license='Apache 2.0',
+
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache Software License',
+            'Topic :: Internet :: WWW/HTTP',
+            'Topic :: Software Development',
+            ],
+
+        requires=[
+            ],
+
+        packages=[
+            ],
+        scripts=[
+            ],
+
+        )
